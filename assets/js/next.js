f9fbb2368a148a0dfa9d38c77cf77e0f50a772c3
LEELA-SRI <leelasric@gmail.com>
2022-03-07 15:37:39 +0530

    add overlay functionality

--- assets/js/next.js
+++ assets/js/next.js
@@ -12,3 +12,24 @@ frameBtn.addEventListener("click", toggleFrame);
 //     frameBtn.classList.toggle('hiddeniframe');
 
 // };
+
+const navBtn = document.querySelector(".bi");
+const overlay = document.querySelector(".overlay");
+const navBar = document.querySelector(".navbar");
+
+console.log(navBtn);
+
+const navFunction = function () {
+  overlay.classList.toggle("hidden");
+  navBtn.classList.toggle("bi-x");
+  navBar.classList.toggle("navbar-mobile");
+};
+const navFunctionClose = function () {
+  overlay.classList.toggle("hidden");
+  navBtn.classList.toggle("bi-x");
+  navBar.classList.toggle("navbar-mobile");
+};
+
+
+navBtn.addEventListener("click", navFunction);
+overlay.addEventListener("click", navFunctionClose);
