5907114dea6146d8afa32c4cec156ae1bd91bacc
LEELA-SRI <leelasric@gmail.com>
2022-03-07 15:37:18 +0530

    update main

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,11 +109,11 @@
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+  // on('click', '.mobile-nav-toggle', function(e) {
+  //   select('#navbar').classList.toggle('navbar-mobile')
+  //   this.classList.toggle('bi-list')
+  //   this.classList.toggle('bi-x')
+  // })
 
   /**
    * Mobile nav dropdowns activate
