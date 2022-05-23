b92589a0bba35b0e529d2ee51ef1d1fc34d957c8
Sriram <71959217+Sriram-bb63@users.noreply.github.com>
2022-05-23 15:57:23 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'leelasric@gmail.com';
+  $receiving_email_address = 'roboticsclubsist@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
